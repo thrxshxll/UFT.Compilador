f081e9f6f302ae22132e6b56460d095dbd8638be
Yuri Nascimento <yurinascimennto@gmail.com>
2024-10-03 20:21:56 -0300

    up de uns arquivos não muito importantes

--- src/translator/out.py
+++ src/translator/out.py
@@ -1,10 +1,23 @@
+max = 100
 
-def even():
-    num = 1
-    while num <= 16:
-        if num % 2 == 0:
-            print(num)
-        num = num + 1
+def checkprime(ret, i, arg):
+    i = 2
+    while i < arg:
+        if arg % i == 0:
+            ret = 0
+            i = arg
+        i = i + 1
 
-even()
+
+def primes(max):
+    i = 0
+    arg = 2
+    while arg < max:
+        ret = 1
+        checkprime(ret, i, arg)
+        if ret == 1:
+            print(arg)
+        arg = arg + 1
+
+primes(max)
 
